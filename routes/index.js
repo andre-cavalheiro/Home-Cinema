747a56d3bbe931756305cf3fe8d6e797aed44483
andre <andre.cavalheiro@tecnico.ulisboa.pt>
2017-06-08 15:14:05 +0000

    Alguns melhoramentos

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ var router = express.Router();
 var movie_library = require("../public/videos/movies_data.json");
 
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index.ejs', { movies: movie_library });
 });
 
 module.exports = router;
-
-//document.getElementById('form_id').action;
--- routes/torrent.js
+++ routes/torrent.js
@@ -1,12 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({ dest: '/tmp/' });
+var fs = require('fs');
+var movie_library = require("../public/videos/movies_data.json");
 
+
+
+//Configuração das opções de salvaguarda dos ficheiros:
+var storageOpts = multer.diskStorage({
+    destination: function(req, file, cb) {
+        var dest = __dirname + "/../public/videos/";
+        cb(null, dest);
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+var upload = multer({
+    storage: storageOpts,
+    limits: {
+        fileSize: 256 * 1024 * 1024 //Alterar??
+    },
+    fileFilter: function(req, file, cb) {
+        name = file.originalname;
+        console.log('Nome:' + file.originalname);
+        if ((name.substring(name.length - 8, name.length)) != ".torrent") {
+            console.log("File rejected: " + (name.substring(name.length - 8, name.length)));
+            cb(null, false);
+        } else {
+            console.log("File accpted");
+            cb(null, true);
+        }
+    }
+});
+
+
+//Resposta ao POST request
 router.post('/torrent', upload.any(), function(req, res) {
     console.log(req.body, 'Body');
-    console.log(req.files, 'files');
-    res.end();
+    console.log(req.files[0], 'files');
+    res.end("File uploaded (maybe)");
 });
 
 module.exports = router;
