3a7af55099415574b481f162dab4e5b43073cc00
andre <andre.cavalheiro@tecnico.ulisboa.pt>
2017-07-10 06:14:47 +0000

    cinema a funcionar

--- routes/cinema.js
+++ routes/cinema.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var library = require("../public/videos/movies_data.json");
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-    res.render('cinema', { id: req.params.id });
+    res.render('cinema', { movie: library.library[req.params.id - 1] });
 });
 
 module.exports = router;
