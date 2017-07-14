e9d4f96943a0cf776c3c2fb08c72f57c610b8c30
andre <andre.cavalheiro@tecnico.ulisboa.pt>
2017-07-14 20:17:05 +0000

    Melhoramentos para streaming com mais tipos de ficheiro

--- routes/stream.js
+++ routes/stream.js
@@ -1,29 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-//Falta verificar se o ficheiro escolhido é de video
-var getLargestFile = function (torrent) {
-    var file;
-    for(i = 0; i < torrent.files.length; i++) {
-        if (!file || file.length < torrent.files[i].length) {
-            file = torrent.files[i];
-        }
-    }
-    return file;
-};
-
 
 router.get('/:index/:infohash', function(req, res, next) {
     var torrent = 'magnet:?xt=urn:btih:' + req.params.infohash;
     var client = req.app.client;
     try {
         var torrent = client.get(torrent);
+        var file = torrent.files[req.params.index]
 
-        var file = getLargestFile(torrent);
-
-        console.log("Ficheiro escolhido: " +file.name);
+        console.log("Ficheiro escolhido: " + file.name);
         var total = file.length;
-        if(typeof req.headers.range != 'undefined') {
+        if (typeof req.headers.range != 'undefined') {
             var range = req.headers.range;
             var parts = range.replace(/bytes=/, "").split("-");
             var partialstart = parts[0];
@@ -32,11 +20,17 @@ router.get('/:index/:infohash', function(req, res, next) {
             var end = partialend ? parseInt(partialend, 10) : total - 1;
             var chunksize = (end - start) + 1;
         } else {
-            var start = 0; var end = total;
+            var start = 0;
+            var end = total;
         }
-        var stream = file.createReadStream({start: start, end: end});
-        res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length':
-chunksize, 'Content-Type': 'video/mkv' });
+        var stream = file.createReadStream({ start: start, end: end });
+        var type = 'video/' + file.name.substring(file.name.length - 3, file.name.length);
+        res.writeHead(206, {
+            'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': type
+        });
         stream.pipe(res);
     } catch (err) {
         res.status(500).send('Error: ' + err.toString());
