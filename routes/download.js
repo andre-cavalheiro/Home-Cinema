d615b82202db74eb2d17832f9aceb1003a34250e
andre <andre.cavalheiro@tecnico.ulisboa.pt>
2017-07-18 19:42:12 +0000

    Melhoramento nos error handelings de receber o torrent

--- routes/download.js
+++ routes/download.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var getLargestFile = function (torrent) {
+var getLargestFile = function(torrent) {
     var file;
     var index;
-    for(i = 0; i < torrent.files.length; i++) {
+    for (i = 0; i < torrent.files.length; i++) {
         if (!file || file.length < torrent.files[i].length) {
             file = torrent.files[i];
-            index=i;
+            index = i;
         }
     }
     return index;
@@ -15,22 +15,36 @@ var getLargestFile = function (torrent) {
 
 
 router.post('/download', function(req, res, next) {
-      var magnet = req.body.magnetURI;
-      var client = req.app.client;
-      //Falta verificar se o torrent já esta a ser baixado ou nao!
-      try{
-        console.log("Iniciar proceso de Download");
-        client.add(magnet,{ path: __dirname + '/../public/videos/' }, function(torrent){
-            console.log('Downloading:', torrent.infoHash)
-            var index=getLargestFile(torrent);
-            var type = torrent.files[index].name.substring(torrent.files[index].name.length-3,torrent.files[index].name.length);
+    var magnet = req.body.magnetURI;
+    var client = req.app.client;
+    var exists = 0;
+    //Verify if the requested torrent is already being downloaded
+    for (var i = 0; i < client.torrents.length; ixx) {
+        if (magnet.indexOf(client.torrents[i].infoHash) != -1) {
+            exists = 1;
+            torrent = client.torrents[i];
+            var index = getLargestFile(torrent);
+            var type = torrent.files[index].name.substring(torrent.files[index].name.length - 3, torrent.files[index].name.length);
             console.log(type)
-            res.render('download',{state: 1 , infohash:torrent.infoHash,index:index,type:type});
+            res.render('download', { state: 1, infohash: torrent.infoHash, index: index, type: type });
+            break;
+        }
+    }
+    if (exists == 0) {
+        //Download
+        client.add(magnet, { path: __dirname + '/../public/videos/' }, function(torrent) {
+                console.log('Downloading:', torrent.infoHash)
+                var index = getLargestFile(torrent);
+                var type = torrent.files[index].name.substring(torrent.files[index].name.length - 3, torrent.files[index].name.length);
+                console.log(type)
+                    //Falta verificar se é o tipo de um video! E se não parar o download!
+                res.render('download', { state: 1, infohash: torrent.infoHash, index: index, type: type });
+            })
+            //Error handling:
+        client.on('error', function(err) {
+            console.log("Error Downloading: ")
+            res.render('download', { state: 0, infohash: "unavailable" });
         })
-      }catch (err) {
-        //Falta resolver para quando o magnet não é valido!
-        console.log("Error Downloading: ")
-        res.render('download',{state: 0 , infohash:"unavailable"});
     }
 });
 
