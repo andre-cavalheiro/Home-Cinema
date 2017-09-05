4f608731ace5e301c540828afca7313f18e18fd3
andre <andre.cavalheiro@tecnico.ulisboa.pt>
2017-09-05 00:10:37 +0000

    Inicio das alteracoes

--- routes/download.js
+++ routes/download.js
@@ -21,12 +21,9 @@ router.post('/download', function(req, res, next) {
     //Verify if the requested torrent is already being downloaded
     for (var i = 0; i < client.torrents.length; i++) {
         if (magnet.indexOf(client.torrents[i].infoHash) != -1) {
-            exists = 1;
             torrent = client.torrents[i];
-            var index = getLargestFile(torrent);
-            var type = torrent.files[index].name.substring(torrent.files[index].name.length - 3, torrent.files[index].name.length);
-            console.log(type)
-            res.render('download', { state: 1, infohash: torrent.infoHash, index: index, type: type });
+            exists = 1;
+            res.status(200).send({ infoHash: torrent.infoHash });
             break;
         }
     }
@@ -35,16 +32,11 @@ router.post('/download', function(req, res, next) {
         //Download
         client.add(magnet, { path: __dirname + '/../public/videos/' }, function(torrent) {
                 console.log('Downloading:', torrent.infoHash)
-                var index = getLargestFile(torrent);
-                var type = torrent.files[index].name.substring(torrent.files[index].name.length - 3, torrent.files[index].name.length);
-                console.log(type)
-                    //Falta verificar se é o tipo de um video! E se não parar o download!
-                res.render('download', { state: 1, infohash: torrent.infoHash, index: index, type: type });
+                res.status(200).send({ infoHash: torrent.infoHash });
             })
             //Error handling:
         client.on('error', function(err) {
-            console.log("Error Downloading: ")
-            res.render('download', { state: 0, infohash: "unavailable" });
+            res.status(500).send('Error Downloading:' + err);
         })
     }
 });
