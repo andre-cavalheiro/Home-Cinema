1554a98873cbda2121ce239d736d30c07566fcc2
andre <andre.cavalheiro@tecnico.ulisboa.pt>
2017-09-20 23:31:11 +0000

    fixed bug

--- app.js
+++ app.js
@@ -30,14 +30,13 @@ app.library = [{ infoHash: -1, time: -1, limit: -1 }]
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 //Answering requestes
 app.use('/', index);
@@ -58,41 +57,55 @@ app.use('/stream/head', head);
 app.use('/stream/video', video)
 app.use('/stream/metadata', metadata)
 app.use('/stream/files', files);
-
+app.get('/stream/remove/:infoHash', function(req, res) {
+    var remove = app.removeTorrent(req.params.infoHash);
+    if (remove == 1) {
+        res.status(200).send("File removed")
+    } else {
+        res.status(500).send("Error deleting file")
+    }
+})
 
 
 //Global functions 
 app.removeTorrent = function(infoHash) {
     var torrent = app.client.get(infoHash);
     console.log("Eliminate file:")
-    rimraf(path.normalize(torrent.path + torrent.name), function(error) {
-        console.log('Error: ', error);
-    });
-    console.log("clean library")
-    for (i = 0; i < app.library.length; i++) {
-        if (app.library[i].infoHash == infoHash) {
-            app.library[i].infoHash = -1;
-            app.library[i].time = -1;
-            app.library[i].limit = -1;
+    try {
+        rimraf(path.normalize(torrent.path + torrent.name), function(error) {
+            console.log('Error: ', error);
+        });
+        console.log("clean library")
+        for (i = 0; i < app.library.length; i++) {
+            if (app.library[i].infoHash == infoHash) {
+                app.library[i].infoHash = -1;
+                app.library[i].time = -1;
+                app.library[i].limit = -1;
+            }
         }
-    }
+        app.client.remove(infoHash);
+        console.log("Removed")
+        return 1;
 
-    app.client.remove(infoHash);
-    console.log("Removed")
+    } catch (e) {
+        console.log("Error while torrent was being removed");
+        return 0;
+    }
 }
 
 setInterval(function() {
     var now = Date.now();
     for (i = 0; i < app.library.length; i++) {
-        console.log(app.library)
+        //console.log(app.library)
         if (app.library[i].time > 0) {
             if ((now - app.library[i].time) > app.library[i].limit) {
-                console.log("Diff: " + (now - app.library[i].time))
+                //console.log("Diff: " + (now - app.library[i].time))
                 app.removeTorrent(app.library[i].infoHash);
             }
         }
     }
 }, 1000);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
